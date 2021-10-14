465092e5f9cfa295b5a3fb6c72d5fd9c57439ff1
yabhi1205 <83153764+yabhi1205@users.noreply.github.com>
2021-10-14 15:56:33 +0530

    booklist

---
+++ js/booklist.js
@@ -0,0 +1,84 @@
+let a = {
+    first: {
+        subjects: ["mathematics", "chemistry", "physics", "PPS", "electrical", "electronics"],
+        mathematics: [
+            ["https://images-na.ssl-images-amazon.com/images/I/51lvg-0KNhL._SX331_BO1,204,203,200_.jpg", "https://www.amazon.in/Textbook-Engineering-Mathematics-Sem-I/dp/8131808084/ref=sr_1_1_sspa?dchild=1&keywords=maths+1&qid=1634196107&s=books&sr=1-1-spons&psc=1&smid=A15DBATYR506U3&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExWjdKUk1PMFNLM0VWJmVuY3J5cHRlZElkPUEwMDMwMzM4QTJMVjdHQk5TNFZGJmVuY3J5cHRlZEFkSWQ9QTA3NTc4OTUyWFFQUUE2UjdOSlgzJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="],
+            ["https://images-eu.ssl-images-amazon.com/images/I/51JRhJH2dXL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Textbook-Engineering-Mathematics-Polytechnic-Students/dp/8123919425/ref=sr_1_3?dchild=1&keywords=engineering+maths+1&qid=1634196478&s=books&sr=1-3"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/515In6mKGZL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Engineering-Mathematics-Semester-II-Second/dp/9390177952/ref=sr_1_9?dchild=1&keywords=engineering+maths+1&qid=1634196478&s=books&sr=1-9"]
+        ],
+        chemistry: [
+            ["https://images-na.ssl-images-amazon.com/images/I/51UKsTi8jGL._SX381_BO1,204,203,200_.jpg", "https://www.amazon.in/Textbook-Engineering-Chemistry-As-AICTE/dp/8126576324/ref=sr_1_1_sspa?dchild=1&keywords=engineering+chemistry+sem+1&qid=1634196654&s=books&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzSTYwS1pUUkJLV1VMJmVuY3J5cHRlZElkPUEwNTM4MzA1SUs5VzZEUzc1RUhZJmVuY3J5cHRlZEFkSWQ9QTA4ODE3NDVGUUhISlhDNEs1QVEmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/51tTcnAgQUL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/ENGINEERING-CHEMISTRY-Suba-Ramesh-S-Vairam/dp/8126551348/ref=sr_1_5?dchild=1&keywords=engineering+chemistry&qid=1634196803&s=books&sr=1-5"],
+            ["https://images-na.ssl-images-amazon.com/images/I/514bQUrsqQL._SX352_BO1,204,203,200_.jpg", "https://www.amazon.in/Textbook-Engineering-Chemistry-SS-Dara/dp/8121903599/ref=sr_1_7?dchild=1&keywords=engineering+chemistry&qid=1634196803&s=books&sr=1-7"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/511nEhVoLqL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Engineering-Chemistry-India-P-C-Jain/dp/9384378070/ref=sr_1_8?dchild=1&keywords=engineering+chemistry&qid=1634196803&s=books&sr=1-8"]
+        ],
+        physics: [
+            ["https://images-na.ssl-images-amazon.com/images/I/41vgA1RS08L._SX351_BO1,204,203,200_.jpg", "https://www.amazon.in/Textbook-Engineering-Physics-M-Avadhanulu/dp/9352833996/ref=sr_1_3?dchild=1&keywords=engineering+physics&qid=1634196988&s=books&sr=1-3"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/41R5WaPAdwL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Engineering-Physics-KIIT-B-K-Pandey/dp/8131519678/ref=sr_1_4?dchild=1&keywords=engineering+physics&qid=1634196988&s=books&sr=1-4"],
+            ["https://images-na.ssl-images-amazon.com/images/I/51Mjaa3yS3L._SX362_BO1,204,203,200_.jpg", "https://www.amazon.in/Engineering-Physics-Shatendra-Sharma/dp/9386873311/ref=sr_1_5?dchild=1&keywords=engineering+physics&qid=1634196988&s=books&sr=1-5"]
+        ],
+        pps: [
+            ["https://images-na.ssl-images-amazon.com/images/I/41L8Qz69cxL._SX365_BO1,204,203,200_.jpg", "https://www.amazon.in/Programming-C-Ajay-Mittal/dp/8131729346/ref=sr_1_1_sspa?dchild=1&keywords=engineering+c+programming&qid=1634197252&s=books&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyNzJURlQ3OTI3VFMwJmVuY3J5cHRlZElkPUEwOTY2NTQ2MkZKODdQTFdTWDI4SyZlbmNyeXB0ZWRBZElkPUEwMjY4NTI0R0FQMU1YMk80MFpMJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="],
+            ["https://images-na.ssl-images-amazon.com/images/I/31B2YwyoQpL._BO1,204,203,200_.jpg", "https://www.amazon.in/Fundamentals-Engineering-Programming-C-Fortran/dp/0521629500/ref=sr_1_3?dchild=1&keywords=engineering+c+programming&qid=1634197252&s=books&sr=1-3"],
+            ["https://images-na.ssl-images-amazon.com/images/I/514P9GAKY3L._SX384_BO1,204,203,200_.jpg", "https://www.amazon.in/Engineering-Problem-Solving-Delores-Etter/dp/0130109304/ref=sr_1_8?dchild=1&keywords=engineering+c+programming&qid=1634197252&s=books&sr=1-8"]
+        ],
+        electrical: [
+            ["https://images-eu.ssl-images-amazon.com/images/I/41C31Im5ajL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Fundamentals-Electrical-Engineering-Charles-Gross/dp/1439837147/ref=sr_1_1_sspa?dchild=1&keywords=engineering+electrical&qid=1634197411&s=books&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMUlCSkgxRUNIUFhFJmVuY3J5cHRlZElkPUEwNDE0NDE4MkM1NVdCRFVKTUdFNyZlbmNyeXB0ZWRBZElkPUEwNzI0NDkxMjRRQlMyVDFJMkQ3OCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="],
+            ["https://images-eu.ssl-images-amazon.com/images/I/41VgfPTTW7L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Basic-Electrical-Engineering-V-K-Mehta/dp/812190871X/ref=sr_1_3?dchild=1&keywords=engineering+electrical&qid=1634197411&s=books&smid=AT95IG9ONZD7S&sr=1-3"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/41C31Im5ajL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Fundamentals-Electrical-Engineering-Charles-Gross/dp/1439837147/ref=sr_1_1_sspa?dchild=1&keywords=engineering+electrical&qid=1634197411&s=books&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMUlCSkgxRUNIUFhFJmVuY3J5cHRlZElkPUEwNDE0NDE4MkM1NVdCRFVKTUdFNyZlbmNyeXB0ZWRBZElkPUEwNzI0NDkxMjRRQlMyVDFJMkQ3OCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="]
+        ],
+        electronics: [
+            ["https://images-eu.ssl-images-amazon.com/images/I/51HpfGEoCjL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Basics-Electronics-Engineering-Wiley-India/dp/8126550775/ref=sr_1_2_sspa?dchild=1&keywords=engineering+electronics&qid=1634197538&s=books&sr=1-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyTk40NzdXNURNSE8xJmVuY3J5cHRlZElkPUEwNTAwNzI2VkJFMTY3WjVVTE85JmVuY3J5cHRlZEFkSWQ9QTA2NTg3MDRXOE9UNVpZQkdHN1Mmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/51apMp-coYL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Electronic-Devices-Circuit-Boylestad-Nashelsky/dp/9332542600/ref=sr_1_1_sspa?dchild=1&keywords=engineering+electronics+sem+1&qid=1634197614&s=books&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFHRUhHMEYwVVBaS1QmZW5jcnlwdGVkSWQ9QTAzNTAzMThTUFhJRDJQQ1MzS08mZW5jcnlwdGVkQWRJZD1BMDU1MjYwMzExRjQxUzFZUzkzS0Mmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl"]
+        ]
+    },
+    second: {
+        subjects: ["mathsII", "chemistry", "physics", "pps", "electrical", "electronics"],
+        mathsII: [
+            ["https://images-na.ssl-images-amazon.com/images/I/517svMUSTML._SX331_BO1,204,203,200_.jpg", "https://www.amazon.in/Textbook-Engineering-Mathematics-technical-Universities/dp/9380386788/ref=sr_1_4?dchild=1&keywords=btech+maths+2&qid=1634182386&sr=8-4", "engineering mathematics-2"],
+        ],
+        chemistry: [],
+        physics: [],
+        pps: [],
+        electrical: [],
+        electronics: []
+    }
+}
+function books(sem,subject) {
+    let all = ""
+    var assume=`a.${sem}.${subject}`
+    bc=assume.valueOf()
+    console.log(bc)
+    console.log(a.first.chemistry)
+    assume.valueOf().forEach(element => {
+        console.log(element[1])
+        const bookstr = `
+    <div class="book1">
+        <img src="${element[0]}" class="center" width="150px">
+        <figcaption>
+        <a
+            href="${element[1]}">Engineering
+            mathematics part-2</a>
+        </figcaption>
+    </div>`
+        all = all + bookstr
+    });
+    books_list.innerHTML = all
+}
+
+function buttons(){
+    let sub=a.first.subjects
+    console.log(sub)
+    substr =`
+    <button id="all">all</button>
+    <button id="sub1">${sub[0]}</button>
+    <button id="sub2">${sub[1]}</button>
+    <button id="sub3">${sub[2]}</button>
+    <button id="sub4">${sub[3]}</button>
+    <button id="sub5">${sub[4]}</button>
+    <button id="sub6">${sub[5]}</button>`
+    button_bar.innerHTML = substr
+}
+
+buttons()
+books("first","chemistry")
