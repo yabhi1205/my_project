d8d8b05b5888261b7bdeaf043e9a78171d1917e9
yabhi1205 <83153764+yabhi1205@users.noreply.github.com>
2021-10-18 09:26:10 +0530

    Update booklist.js

--- js/booklist.js
+++ js/booklist.js
@@ -33,18 +33,38 @@ let a = {
         ]
     },
     second: {
-        subjects: ["mathsII", "chemistry", "physics", "pps", "electrical", "electronics"],
+        subjects: ["mathsII", "chemistry", "physics", "PPS", "electrical", "electronics"],
         mathsII: [
             ["https://images-na.ssl-images-amazon.com/images/I/517svMUSTML._SX331_BO1,204,203,200_.jpg", "https://www.amazon.in/Textbook-Engineering-Mathematics-technical-Universities/dp/9380386788/ref=sr_1_4?dchild=1&keywords=btech+maths+2&qid=1634182386&sr=8-4", "engineering mathematics-2"],
         ],
-        chemistry: [],
-        physics: [],
-        pps: [],
-        electrical: [],
-        electronics: []
+        chemistry: [
+            ["https://images-na.ssl-images-amazon.com/images/I/51UKsTi8jGL._SX381_BO1,204,203,200_.jpg", "https://www.amazon.in/Textbook-Engineering-Chemistry-As-AICTE/dp/8126576324/ref=sr_1_1_sspa?dchild=1&keywords=engineering+chemistry+sem+1&qid=1634196654&s=books&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzSTYwS1pUUkJLV1VMJmVuY3J5cHRlZElkPUEwNTM4MzA1SUs5VzZEUzc1RUhZJmVuY3J5cHRlZEFkSWQ9QTA4ODE3NDVGUUhISlhDNEs1QVEmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/51tTcnAgQUL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/ENGINEERING-CHEMISTRY-Suba-Ramesh-S-Vairam/dp/8126551348/ref=sr_1_5?dchild=1&keywords=engineering+chemistry&qid=1634196803&s=books&sr=1-5"],
+            ["https://images-na.ssl-images-amazon.com/images/I/514bQUrsqQL._SX352_BO1,204,203,200_.jpg", "https://www.amazon.in/Textbook-Engineering-Chemistry-SS-Dara/dp/8121903599/ref=sr_1_7?dchild=1&keywords=engineering+chemistry&qid=1634196803&s=books&sr=1-7"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/511nEhVoLqL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Engineering-Chemistry-India-P-C-Jain/dp/9384378070/ref=sr_1_8?dchild=1&keywords=engineering+chemistry&qid=1634196803&s=books&sr=1-8"],
+        ],
+        physics: [
+            ["https://images-na.ssl-images-amazon.com/images/I/41vgA1RS08L._SX351_BO1,204,203,200_.jpg", "https://www.amazon.in/Textbook-Engineering-Physics-M-Avadhanulu/dp/9352833996/ref=sr_1_3?dchild=1&keywords=engineering+physics&qid=1634196988&s=books&sr=1-3"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/41R5WaPAdwL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Engineering-Physics-KIIT-B-K-Pandey/dp/8131519678/ref=sr_1_4?dchild=1&keywords=engineering+physics&qid=1634196988&s=books&sr=1-4"],
+            ["https://images-na.ssl-images-amazon.com/images/I/51Mjaa3yS3L._SX362_BO1,204,203,200_.jpg", "https://www.amazon.in/Engineering-Physics-Shatendra-Sharma/dp/9386873311/ref=sr_1_5?dchild=1&keywords=engineering+physics&qid=1634196988&s=books&sr=1-5"]
+        ],
+        PPS: [
+            ["https://images-na.ssl-images-amazon.com/images/I/41L8Qz69cxL._SX365_BO1,204,203,200_.jpg", "https://www.amazon.in/Programming-C-Ajay-Mittal/dp/8131729346/ref=sr_1_1_sspa?dchild=1&keywords=engineering+c+programming&qid=1634197252&s=books&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyNzJURlQ3OTI3VFMwJmVuY3J5cHRlZElkPUEwOTY2NTQ2MkZKODdQTFdTWDI4SyZlbmNyeXB0ZWRBZElkPUEwMjY4NTI0R0FQMU1YMk80MFpMJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="],
+            ["https://images-na.ssl-images-amazon.com/images/I/31B2YwyoQpL._BO1,204,203,200_.jpg", "https://www.amazon.in/Fundamentals-Engineering-Programming-C-Fortran/dp/0521629500/ref=sr_1_3?dchild=1&keywords=engineering+c+programming&qid=1634197252&s=books&sr=1-3"],
+            ["https://images-na.ssl-images-amazon.com/images/I/514P9GAKY3L._SX384_BO1,204,203,200_.jpg", "https://www.amazon.in/Engineering-Problem-Solving-Delores-Etter/dp/0130109304/ref=sr_1_8?dchild=1&keywords=engineering+c+programming&qid=1634197252&s=books&sr=1-8"]
+        ],
+        electrical: [
+            ["https://images-eu.ssl-images-amazon.com/images/I/41C31Im5ajL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Fundamentals-Electrical-Engineering-Charles-Gross/dp/1439837147/ref=sr_1_1_sspa?dchild=1&keywords=engineering+electrical&qid=1634197411&s=books&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMUlCSkgxRUNIUFhFJmVuY3J5cHRlZElkPUEwNDE0NDE4MkM1NVdCRFVKTUdFNyZlbmNyeXB0ZWRBZElkPUEwNzI0NDkxMjRRQlMyVDFJMkQ3OCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="],
+            ["https://images-eu.ssl-images-amazon.com/images/I/41VgfPTTW7L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Basic-Electrical-Engineering-V-K-Mehta/dp/812190871X/ref=sr_1_3?dchild=1&keywords=engineering+electrical&qid=1634197411&s=books&smid=AT95IG9ONZD7S&sr=1-3"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/41C31Im5ajL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Fundamentals-Electrical-Engineering-Charles-Gross/dp/1439837147/ref=sr_1_1_sspa?dchild=1&keywords=engineering+electrical&qid=1634197411&s=books&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMUlCSkgxRUNIUFhFJmVuY3J5cHRlZElkPUEwNDE0NDE4MkM1NVdCRFVKTUdFNyZlbmNyeXB0ZWRBZElkPUEwNzI0NDkxMjRRQlMyVDFJMkQ3OCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="]
+        ],
+        electronics: [
+            ["https://images-eu.ssl-images-amazon.com/images/I/51HpfGEoCjL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Basics-Electronics-Engineering-Wiley-India/dp/8126550775/ref=sr_1_2_sspa?dchild=1&keywords=engineering+electronics&qid=1634197538&s=books&sr=1-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyTk40NzdXNURNSE8xJmVuY3J5cHRlZElkPUEwNTAwNzI2VkJFMTY3WjVVTE85JmVuY3J5cHRlZEFkSWQ9QTA2NTg3MDRXOE9UNVpZQkdHN1Mmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl"],
+            ["https://images-eu.ssl-images-amazon.com/images/I/51apMp-coYL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg", "https://www.amazon.in/Electronic-Devices-Circuit-Boylestad-Nashelsky/dp/9332542600/ref=sr_1_1_sspa?dchild=1&keywords=engineering+electronics+sem+1&qid=1634197614&s=books&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFHRUhHMEYwVVBaS1QmZW5jcnlwdGVkSWQ9QTAzNTAzMThTUFhJRDJQQ1MzS08mZW5jcnlwdGVkQWRJZD1BMDU1MjYwMzExRjQxUzFZUzkzS0Mmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl"]
+        ]
     }
 }
-function check(){
+function check() {
     if (localStorage.getItem('bookstr') == null) {
         let all = ""
         return all
@@ -57,7 +77,7 @@ function check(){
 }
 
 function books(sem, subject) {
-    let all=check()
+    let all = check()
     var assume = a[sem]
     bc = assume[subject]
     bc.forEach(element => {
@@ -79,22 +99,22 @@ function books(sem, subject) {
 }
 
 function buttons(sem) {
-    button={1:button1,2:button2,3:button3,4:button4,5:button5,6:button6}
-    let count=1
+    button = { 1: button1, 2: button2, 3: button3, 4: button4, 5: button5, 6: button6 }
+    let count = 1
     let sub = a[sem].subjects
     sub.forEach(element => {
         substr = `${element}`
-        assume=button[count]
+        assume = button[count]
         assume.innerHTML = substr
-        count=count+1
+        count = count + 1
     });
 }
-function all_sub(sem){
-    allSub=[]
+function all_sub(sem) {
+    allSub = []
     sem.forEach(element => {
-        let subs=a[element].subjects
+        let subs = a[element].subjects
         subs.forEach(element1 => {
-            allSub.push([element,element1])
+            allSub.push([element, element1])
         });
     });
     return allSub
@@ -104,117 +124,121 @@ function all(user) {
     user.forEach(bnl => {
         sem = all_sub([bnl])
         sem.forEach(element => {
-            final=books(element[0], element[1])
+            final = books(element[0], element[1])
         });
     });
-    document.getElementById('all').style.backgroundColor="red"
+    document.getElementById('all').style.backgroundColor = "red"
     books_list.innerHTML = final
+    button_update("all")
     localStorage.removeItem("bookstr")
 }
-function single(sem,subject) {
-    final=books(sem,subject)
+function single(sem, subject) {
+    console.log(sem, subject)
+    final = books(sem, subject)
     books_list.innerHTML = final
     localStorage.removeItem("bookstr")
 }
 function button_update(button) {
-    buttonlist=["all","sub1","sub2","sub3","sub4","sub5","sub6"]
+    buttonlist = ["all", "sub1", "sub2", "sub3", "sub4", "sub5", "sub6"]
     buttonlist.forEach(element => {
-        if (element==button) {
-            document.getElementById(button).style.backgroundColor="red"
+        if (element == button) {
+            document.getElementById(button).style.backgroundColor = "red"
         }
-        else{
-            document.getElementById(element).style.backgroundColor="black"
+        else {
+            document.getElementById(element).style.backgroundColor = "black"
         }
     });
 }
-function checkedradio(sem,branch) {
-    document.getElementById(sem).checked=true
-    document.getElementById(branch).checked=true
+function checkedradio(sem, branch) {
+    document.getElementById(sem).checked = true
+    document.getElementById(branch).checked = true
 }
 function checkfilter() {
-    let semlist = { "1": "first", 2: "second", 3: "third", 4: "fourth", 5: "fifth", 6: "sixth",7:"seventh",8:"eighth"}
-    if (localStorage.getItem('final')==null) {
-        elementstr=localStorage.getItem('temp')
-        element=JSON.parse(elementstr)
-        sem=semlist[element.sem]
-        return([sem,element.branch])
+    let semlist = { "1": "first", 2: "second", 3: "third", 4: "fourth", 5: "fifth", 6: "sixth", 7: "seventh", 8: "eighth" }
+    if (localStorage.getItem('final') == null) {
+        elementstr = localStorage.getItem('temp')
+        element = JSON.parse(elementstr)
+        sem = semlist[element.sem]
+        return ([sem, element.branch])
     }
-    else{
-        elementstr=localStorage.getItem('final')
-        element=JSON.parse(elementstr)
-        return([element[0],element[1]])
+    else {
+        elementstr = localStorage.getItem('final')
+        element = JSON.parse(elementstr)
+        return ([element[0], element[1]])
     }
 }
 function update() {
-    element=checkfilter()
-    pta=element[0]
+    element = checkfilter()
+    pta = element[0]
     all([pta])
     buttons(pta)
-    checkedradio(pta,element[1])
+    checkedradio(pta, element[1])
 }
 
-
-element=checkfilter()
-pta=element[0]
-buttonSub=a[pta].subjects
-console.log(element)
+localStorage.removeItem('final')
+element = checkfilter()
+pta = element[0]
+buttonSub = a[pta].subjects
 update()
 buttonAll = document.getElementById('all')
-buttonAll.addEventListener("click", ()=>{
+buttonAll.addEventListener("click", () => {
     all([pta])
     button_update("all")
 })
 button6 = document.getElementById('sub6')
-button6.addEventListener("click", ()=>{
-    single(pta,buttonSub[5])
+button6.addEventListener("click", () => {
+    single(pta, buttonSub[5])
     button_update("sub6")
 })
 button1 = document.getElementById('sub1')
-button1.addEventListener("click", ()=>{
-    console.log(element[0],buttonSub[0])
-    single(pta,buttonSub[0])
+button1.addEventListener("click", () => {
+    single(pta, buttonSub[0])
     button_update("sub1")
 })
 button2 = document.getElementById('sub2')
-button2.addEventListener("click", ()=>{
-    single(pta,buttonSub[1])
+button2.addEventListener("click", () => {
+    console.log(pta, buttonSub[1])
+    single(pta, buttonSub[1])
     button_update("sub2")
 })
 button3 = document.getElementById('sub3')
-button3.addEventListener("click", ()=>{
-    single(pta,buttonSub[2])
+button3.addEventListener("click", () => {
+    single(pta, buttonSub[2])
     button_update("sub3")
 })
 button4 = document.getElementById('sub4')
-button4.addEventListener("click", ()=>{
-    single(pta,buttonSub[3])
+button4.addEventListener("click", () => {
+    single(pta, buttonSub[3])
     button_update("sub4")
 })
 button5 = document.getElementById('sub5')
-button5.addEventListener("click", ()=>{
-    single(pta,buttonSub[4])
+button5.addEventListener("click", () => {
+    single(pta, buttonSub[4])
     button_update("sub5")
 })
 
-filterAdd= document.getElementById('additional')
-filterAdd.addEventListener("click",()=>{
-    semlist=["first",'second','third','fourth','fifth','sixth','seventh','eighth']
-    branchlist=['cse','cse-other','ece','ee','mechanical','civil','chemical','biotech','pharmacy']
-    for (let index = 0; index < semlist.length; index++){
+filterAdd = document.getElementById('additional')
+filterAdd.addEventListener("click", () => {
+    semlist = ["first", 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth']
+    branchlist = ['cse', 'cse-other', 'ece', 'ee', 'mechanical', 'civil', 'chemical', 'biotech', 'pharmacy']
+    for (let index = 0; index < semlist.length; index++) {
         articlestr = document.getElementById(semlist[index])
         if (articlestr.checked == true) {
-            finalSem=semlist[index]
+            finalSem = semlist[index]
             break
         }
     }
     for (let index = 0; index < branchlist.length; index++) {
         articlestr = document.getElementById(branchlist[index])
         if (articlestr.checked == true) {
-            finalbranch=branchlist[index]
+            finalbranch = branchlist[index]
             break
         }
     }
-    final=[finalSem,finalbranch]
-    localStorage.setItem('final',JSON.stringify(final))
+    final = [finalSem, finalbranch]
+    localStorage.setItem('final', JSON.stringify(final))
+    element = checkfilter()
+    pta = element[0]
+    buttonSub = a[pta].subjects
     update()
 })
